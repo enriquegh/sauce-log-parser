80195af799dd1111aec43c1bcc9d58bd6842591f
Enrique Gonzalez <enrique@saucelabs.com>
2017-06-28 13:30:59 -0700

    Added module to control log file downloads from Sauce

---
+++ log_collector.py
@@ -0,0 +1,23 @@
+"""Gets log from Sauce Labs"""
+
+import httplib2
+import base64
+
+URL_BASE = "https://saucelabs.com/rest/v1/{username}/jobs/{job_id}/assets/log.json"
+
+
+def get_log(admin, access_key, username, job_id):
+    "Obtain log with username and job_id"
+
+    url = URL_BASE.format(username=username, job_id=job_id)
+    print url
+
+    http_conn = httplib2.Http()
+    # http_conn.add_credentials(admin, access_key)
+    user_pass = base64.b64encode("{}:{}".format(admin, access_key))
+    headers = {'Authorization' : 'Basic {}'.format(user_pass)}
+    _, response = http_conn.request(url, method="GET", headers=headers)
+
+    print response
+
+
