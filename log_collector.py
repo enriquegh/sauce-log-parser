b156ce803be44351728ae50fc91a8d0a820af868
Enrique Gonzalez <enrique@saucelabs.com>
2017-06-28 13:41:22 -0700

    log is now saved to a file with format compliant to examine

--- log_collector.py
+++ log_collector.py
@@ -12,11 +12,14 @@ def get_log(admin, access_key, username, job_id):
 
     url = URL_BASE.format(username=username, job_id=job_id)
     print url
-
+    log_name = "log_{}.log".format(job_id)
     http_conn = httplib2.Http()
     # http_conn.add_credentials(admin, access_key)
     user_pass = base64.b64encode("{}:{}".format(admin, access_key))
     headers = {'Authorization' : 'Basic {}'.format(user_pass)}
     _, response = http_conn.request(url, method="GET", headers=headers)
 
+
+    with open(log_name, 'w') as log:
+        log.write(response)
     print response
