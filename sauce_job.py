041cb389834f63064529db720a49aab5429197ff
Enrique Gonzalez <enrique@saucelabs.com>
2019-11-12 08:27:41 -0800

    change HTTP client to requests Prior to this change, httplib2 was used to get the job data
    This change moves this to requests to be able to get more information about the request

--- sauce_job.py
+++ sauce_job.py
@@ -27,7 +27,7 @@ class Job(object):
         """Downloads log"""
         response = log_collector.get_log(api_endpoint, admin, access_key,
                                          username, self.job_id, write)
-        self.data = json.loads(response.decode())
+        self.data = json.loads(response)
 
     def read_data(self, command):
         """Reads data and returns max, min, mean and total"""
