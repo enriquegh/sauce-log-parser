27a7f9741c535a16751f5e85eede7f86e6b51707
Enrique Gonzalez <enrique@saucelabs.com>
2019-11-12 08:27:41 -0800

    change HTTP client to requests Prior to this change, httplib2 was used to get the job data
    This change moves this to requests to be able to get more information about the request

--- log_collector.py
+++ log_collector.py
@@ -1,7 +1,7 @@
 """Gets log from Sauce Labs"""
 
 import base64
-import httplib2
+import requests
 
 
 URL_BASE = "{api_endpoint}/{username}/jobs/{job_id}/assets/log.json"
@@ -13,14 +13,9 @@ def get_log(api_endpoint, admin, access_key, username, job_id, write=False):
     url = URL_BASE.format(api_endpoint=api_endpoint, username=username,
                           job_id=job_id)
     log_name = "log_{}.log".format(job_id)
-    http_conn = httplib2.Http()
-    # http_conn.add_credentials(admin, access_key)
-    user_pass = base64.b64encode('{}:{}'.format(admin, access_key)
-                                 .encode('ascii'))
-    headers = {'Authorization': 'Basic {}'.format(user_pass.decode('ascii'))}
-    _, response = http_conn.request(url, method="GET", headers=headers)
+    resp = requests.get(url, auth=(username, access_key))
 
     if write:
         with open(log_name, 'w') as log:
-            log.write(response.decode('ascii'))
-    return response
+            log.write(resp.text)
+    return resp.text
