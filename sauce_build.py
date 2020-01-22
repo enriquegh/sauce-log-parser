b4f47ec89c566d2a44880b6decde396313ffe0e0
Enrique Gonzalez <enrique@saucelabs.com>
2020-01-22 13:05:19 -0800

    WIP: Add basic job id fetching logic to Build class

---
+++ sauce_build.py
@@ -0,0 +1,43 @@
+import sauce_job
+import requests
+
+
+class BuildNotFound(Exception):
+    """Build was not present or the build does not exist"""
+    pass
+
+
+class SomethingWentWrong(Exception):
+    """A non 200 HTTP status was returned.  Something bad may have happened"""
+    pass
+
+
+class Build():
+    """docstring for Build."""
+
+    URL_BASE = "{api_endpoint}/builds/{build_id}/"
+    URL_JOBS = URL_BASE + "/jobs"
+
+    def __init__(self, api_endpoint, build_id):
+        self.api_endpoint = api_endpoint
+        self.build_id = build_id
+        self.job_list = []
+
+    def get_build_id(self):
+        return self.build_id
+
+    def get_job_list(self):
+        return self.job_list
+
+    def fetch_job_ids(self, admin, access_key):
+        url = self.URL_JOBS.format(api_endpoint=self.api_endpoint,
+                                   build_id=self.build_id)
+
+        resp = requests.get(url, auth=(admin, access_key))
+
+        if resp.status_code == 400:
+            raise BuildNotFound
+        if resp.status_code != 200:
+            raise SomethingWentWrong
+
+        json_resp = resp.json()
